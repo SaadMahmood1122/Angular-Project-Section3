7226cfb37a4c028a8da6873a0f5f87e86866f63f
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-09 20:20:07 +0500

    make all standalone component non standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-// import { HeaderComponent } from './app/header.component';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
-// bootstrapApplication(HeaderComponent);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+// calling the import ftn
+platformBrowserDynamic().bootstrapModule(AppModule);
