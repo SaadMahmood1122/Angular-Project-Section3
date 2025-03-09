764d8e10ae8eff3224e8d4063b2d87fdaebe7ac6
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-09 21:25:14 +0500

    created module of cart and shared with other module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,21 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { MiniTaskComponent } from './task/mini-task/mini-task.component';
 import { FormsModule } from '@angular/forms';
-import { CartComponent } from './shared/cart/cart.component';
+
 import { DatePipe } from '@angular/common';
 import { NewTaskComponent } from './task/new-task/new-task.component';
+import { sharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CartComponent,
     TaskComponent,
     NewTaskComponent,
     MiniTaskComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, sharedModule],
   providers: [DatePipe], // Correctly provide DatePipe in providers array
   bootstrap: [AppComponent],
 })
